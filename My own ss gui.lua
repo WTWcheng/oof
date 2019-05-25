f0276a260b9c1d698c3da95b8012f00644508c8a
Chrisgianneas <37269245+Chrisgianneas@users.noreply.github.com>
2019-05-26 02:41:38 +0300

    Add files via upload

---
+++ My own ss gui.lua
@@ -0,0 +1,271 @@
+-- Farewell Infortality.
+-- Version: 2.82
+-- Instances:
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local MML = Instance.new("TextButton")
+local TROLLGUI = Instance.new("TextButton")
+local HUNGRYNOOB = Instance.new("TextButton")
+local SNIPER = Instance.new("TextButton")
+local MEMEUS = Instance.new("TextButton")
+local THANOS = Instance.new("TextButton")
+local Label = Instance.new("TextLabel")
+local DORITOS = Instance.new("TextButton")
+local MLGGUN = Instance.new("TextButton")
+local PAN = Instance.new("TextButton")
+local TOOKTHEKIDS = Instance.new("TextButton")
+--Properties:
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.new(0.337255, 0.337255, 0.337255)
+Frame.BorderColor3 = Color3.new(0, 0, 0)
+Frame.BorderSizePixel = 5
+Frame.Position = UDim2.new(0.53475517, 0, 0.179591849, 0)
+Frame.Size = UDim2.new(0, 484, 0, 226)
+
+MML.Name = "MML"
+MML.Parent = Frame
+MML.BackgroundColor3 = Color3.new(1, 1, 1)
+MML.Position = UDim2.new(0.0185950398, 0, 0.172566369, 0)
+MML.Size = UDim2.new(0, 104, 0, 21)
+MML.Font = Enum.Font.SourceSans
+MML.Text = "MML"
+MML.TextColor3 = Color3.new(0, 0, 0)
+MML.TextSize = 14
+
+TROLLGUI.Name = "TROLLGUI"
+TROLLGUI.Parent = Frame
+TROLLGUI.BackgroundColor3 = Color3.new(1, 1, 1)
+TROLLGUI.Position = UDim2.new(0.0185950398, 0, 0.336283177, 0)
+TROLLGUI.Size = UDim2.new(0, 104, 0, 21)
+TROLLGUI.Font = Enum.Font.SourceSans
+TROLLGUI.Text = "TROLL GUI"
+TROLLGUI.TextColor3 = Color3.new(0, 0, 0)
+TROLLGUI.TextSize = 14
+
+HUNGRYNOOB.Name = "HUNGRYNOOB"
+HUNGRYNOOB.Parent = Frame
+HUNGRYNOOB.BackgroundColor3 = Color3.new(1, 1, 1)
+HUNGRYNOOB.Position = UDim2.new(0.293388426, 0, 0.172566369, 0)
+HUNGRYNOOB.Size = UDim2.new(0, 104, 0, 21)
+HUNGRYNOOB.Font = Enum.Font.SourceSans
+HUNGRYNOOB.Text = "HUNGRY NOOB"
+HUNGRYNOOB.TextColor3 = Color3.new(0, 0, 0)
+HUNGRYNOOB.TextSize = 14
+
+SNIPER.Name = "SNIPER"
+SNIPER.Parent = Frame
+SNIPER.BackgroundColor3 = Color3.new(1, 1, 1)
+SNIPER.Position = UDim2.new(0.0185950398, 0, 0.840707898, 0)
+SNIPER.Size = UDim2.new(0, 104, 0, 21)
+SNIPER.Font = Enum.Font.SourceSans
+SNIPER.Text = "SNIPER"
+SNIPER.TextColor3 = Color3.new(0, 0, 0)
+SNIPER.TextSize = 14
+
+MEMEUS.Name = "MEMEUS"
+MEMEUS.Parent = Frame
+MEMEUS.BackgroundColor3 = Color3.new(1, 1, 1)
+MEMEUS.Position = UDim2.new(0.0185950398, 0, 0.672566354, 0)
+MEMEUS.Size = UDim2.new(0, 104, 0, 21)
+MEMEUS.Font = Enum.Font.SourceSans
+MEMEUS.Text = "MEMEUS V2"
+MEMEUS.TextColor3 = Color3.new(0, 0, 0)
+MEMEUS.TextSize = 14
+
+THANOS.Name = "THANOS"
+THANOS.Parent = Frame
+THANOS.BackgroundColor3 = Color3.new(1, 1, 1)
+THANOS.Position = UDim2.new(0.0185950398, 0, 0.504424751, 0)
+THANOS.Size = UDim2.new(0, 104, 0, 21)
+THANOS.Font = Enum.Font.SourceSans
+THANOS.Text = "THANOS GLOVE"
+THANOS.TextColor3 = Color3.new(0, 0, 0)
+THANOS.TextSize = 14
+
+Label.Name = "Label"
+Label.Parent = Frame
+Label.BackgroundColor3 = Color3.new(0, 0, 0)
+Label.Position = UDim2.new(0.293388426, 0, 0, 0)
+Label.Size = UDim2.new(0, 200, 0, 18)
+Label.Font = Enum.Font.SourceSans
+Label.Text = "SS GUI"
+Label.TextColor3 = Color3.new(0.980392, 0.980392, 0.980392)
+Label.TextSize = 14
+
+DORITOS.Name = "DORITOS"
+DORITOS.Parent = Frame
+DORITOS.BackgroundColor3 = Color3.new(1, 1, 1)
+DORITOS.Position = UDim2.new(0.293388426, 0, 0.336283177, 0)
+DORITOS.Size = UDim2.new(0, 104, 0, 21)
+DORITOS.Font = Enum.Font.SourceSans
+DORITOS.Text = "DORITOS"
+DORITOS.TextColor3 = Color3.new(0, 0, 0)
+DORITOS.TextSize = 14
+
+MLGGUN.Name = "MLGGUN"
+MLGGUN.Parent = Frame
+MLGGUN.BackgroundColor3 = Color3.new(1, 1, 1)
+MLGGUN.Position = UDim2.new(0.293388426, 0, 0.504424751, 0)
+MLGGUN.Size = UDim2.new(0, 104, 0, 21)
+MLGGUN.Font = Enum.Font.SourceSans
+MLGGUN.Text = "MLG GUN"
+MLGGUN.TextColor3 = Color3.new(0, 0, 0)
+MLGGUN.TextSize = 14
+
+PAN.Name = "PAN"
+PAN.Parent = Frame
+PAN.BackgroundColor3 = Color3.new(1, 1, 1)
+PAN.Position = UDim2.new(0.291322321, 0, 0.672566354, 0)
+PAN.Size = UDim2.new(0, 105, 0, 21)
+PAN.Font = Enum.Font.SourceSans
+PAN.Text = "PAN"
+PAN.TextColor3 = Color3.new(0, 0, 0)
+PAN.TextSize = 14
+
+TOOKTHEKIDS.Name = "TOOKTHEKIDS"
+TOOKTHEKIDS.Parent = Frame
+TOOKTHEKIDS.BackgroundColor3 = Color3.new(1, 1, 1)
+TOOKTHEKIDS.Position = UDim2.new(0.293388426, 0, 0.840707958, 0)
+TOOKTHEKIDS.Size = UDim2.new(0, 104, 0, 21)
+TOOKTHEKIDS.Font = Enum.Font.SourceSans
+TOOKTHEKIDS.Text = "TOOK THE KIDS"
+TOOKTHEKIDS.TextColor3 = Color3.new(0, 0, 0)
+TOOKTHEKIDS.TextSize = 14
+-- Scripts:
+function SCRIPT_DQCG67_FAKESCRIPT() -- MML.Script 
+	local script = Instance.new('Script')
+	script.Parent = MML
+	MML.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/bBDpgSdJ'),true))()
+	end)
+
+end
+coroutine.resume(coroutine.create(SCRIPT_DQCG67_FAKESCRIPT))
+function SCRIPT_CXAW75_FAKESCRIPT() -- TROLLGUI.Script 
+	local script = Instance.new('Script')
+	script.Parent = TROLLGUI
+	TROLLGUI.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/C6FRLy2L'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_CXAW75_FAKESCRIPT))
+function SCRIPT_TTBA72_FAKESCRIPT() -- HUNGRYNOOB.Script 
+	local script = Instance.new('Script')
+	script.Parent = HUNGRYNOOB
+	HUNGRYNOOB.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/9Pc03xEW'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_TTBA72_FAKESCRIPT))
+function SCRIPT_FDOU71_FAKESCRIPT() -- SNIPER.Script 
+	local script = Instance.new('Script')
+	script.Parent = SNIPER
+	SNIPER.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/PACibDFU'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_FDOU71_FAKESCRIPT))
+function SCRIPT_OIUU72_FAKESCRIPT() -- MEMEUS.Script 
+	local script = Instance.new('Script')
+	script.Parent = MEMEUS
+	MEMEUS.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/8n9gehmH'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_OIUU72_FAKESCRIPT))
+function SCRIPT_BKVR89_FAKESCRIPT() -- THANOS.Script 
+	local script = Instance.new('Script')
+	script.Parent = THANOS
+	THANOS.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/9RSq4dUE'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_BKVR89_FAKESCRIPT))
+function SCRIPT_KQRD70_FAKESCRIPT() -- Frame.Script 
+	local script = Instance.new('Script')
+	script.Parent = Frame
+	Frame.Active = true
+	Frame.Draggable = true
+
+end
+coroutine.resume(coroutine.create(SCRIPT_KQRD70_FAKESCRIPT))
+function SCRIPT_LCHI78_FAKESCRIPT() -- DORITOS.Script 
+	local script = Instance.new('Script')
+	script.Parent = DORITOS
+	DORITOS.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/sN2bSACa'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_LCHI78_FAKESCRIPT))
+function SCRIPT_IKQF80_FAKESCRIPT() -- MLGGUN.Script 
+	local script = Instance.new('Script')
+	script.Parent = MLGGUN
+	MLGGUN.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/YaZtZbwE'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_IKQF80_FAKESCRIPT))
+function SCRIPT_ECSC80_FAKESCRIPT() -- PAN.Script 
+	local script = Instance.new('Script')
+	script.Parent = PAN
+	PAN.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/UNbbkRqJ'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_ECSC80_FAKESCRIPT))
+function SCRIPT_LRNS68_FAKESCRIPT() -- TOOKTHEKIDS.Script 
+	local script = Instance.new('Script')
+	script.Parent = TOOKTHEKIDS
+	TOOKTHEKIDS.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet(('https://pastebin.com/raw/d3XNmiDT'),true))()
+	end)
+	
+	
+	
+	
+
+end
+coroutine.resume(coroutine.create(SCRIPT_LRNS68_FAKESCRIPT))
